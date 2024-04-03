7349260f07489d4ad8d38758781cf73a6f0ce1b7
Maria elena Alvarez <164612201+mariaelena30@users.noreply.github.com>
2024-04-03 10:09:01 -0300

    practica
    Signed-off-by: Maria elena Alvarez <164612201+mariaelena30@users.noreply.github.com>

---
+++ lista_espaiado.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const SpacedItemsList());
+
+class SpacedItemsList extends StatelessWidget {
+  const SpacedItemsList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const items = 4;
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 58, 183, 62)),
+        cardTheme: CardTheme(color: Color.fromARGB(255, 136, 66, 128)),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        body: LayoutBuilder(builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: List.generate(
+                    items, (index) => ItemWidget(text: 'Item $index')),
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+class ItemWidget extends StatelessWidget {
+  const ItemWidget({
+    super.key,
+    required this.text,
+  });
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: SizedBox(
+        height: 100,
+        child: Center(child: Text(text)),
+      ),
+    );
+  }
+}
